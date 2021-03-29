b0a2b96d9778a77989661ade824b584607c03922
toishita ryu <r.toishita.sys20@morijyobi.ac.jp>
2021-03-29 12:10:20 +0900

    BMI計算　作成

---
+++ calc_bmi.py
@@ -0,0 +1,4 @@
+def get_area(taizyuu, sinntyou):
+    kekka = taizyuu / sinntyou / sinntyou
+    kekka = kekka * 10000
+    return kekka
---
+++ main.py
@@ -0,0 +1,6 @@
+import calc_bmi
+
+print("BMI計算")
+t = int(input("体重:"))
+s = int(input("身長:"))
+print(f"結果:{calc_bmi.get_area(t,s)}")
